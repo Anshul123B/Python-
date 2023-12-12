8842575ded69fff4a838bb188ec57b4b03babab7
Anshul Bhatt <134294749+Anshul123B@users.noreply.github.com>
2023-12-12 10:40:38 +0530

    Add files via upload

---
+++ membership_operator.py
@@ -0,0 +1,8 @@
+users = [ "Ankit", "Rahul", "Varun", "Alka"]
+
+name = input("Enter your name: ")
+
+if name in users:
+	print("Access granted! Your Welcome")
+else:
+	print("Access deneid! You are not registered")
